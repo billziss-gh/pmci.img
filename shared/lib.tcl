2572f52855f6afd503d50ccf683b34ed984f97c1
Bill Zissimopoulos <billziss@navimatics.com>
2018-08-02 13:44:54 -0700

    refactor common tcl code into shared/lib.tcl

---
+++ shared/lib.tcl
@@ -0,0 +1,57 @@
+# shared/lib.tcl
+#
+# Copyright 2018 Bill Zissimopoulos
+
+# This file is part of "Poor Man's CI".
+#
+# It is licensed under the BSD license. The full license text can be found
+# in the License.txt file at the root of this project.
+
+set timeout 600
+set force_conservative 1
+
+set rootpass "root"
+
+proc spawn_qemu_installer {installer image {arch x86_64} {size 5G}} {
+    spawn qemu-img create -f raw $image $size
+    expect "Formatting"
+    expect eof
+
+    spawn qemu-system-$arch \
+        -smp 2 \
+        -device virtio-scsi \
+        -blockdev driver=file,node-name=diskfile,filename=$image \
+        -blockdev driver=raw,node-name=disk,file=diskfile \
+        -device scsi-hd,drive=disk \
+        -net nic,model=virtio,macaddr=02:00:00:00:00:01 -net user \
+        -cdrom $installer \
+        -boot once=d \
+        -display curses
+
+    return $spawn_id
+}
+
+proc spawn_qemu_image {image {arch x86_64}} {
+    spawn qemu-system-$arch \
+        -smp 2 \
+        -device virtio-scsi \
+        -blockdev driver=file,node-name=diskfile,filename=$image \
+        -blockdev driver=raw,node-name=disk,file=diskfile \
+        -device scsi-hd,drive=disk \
+        -net nic,model=virtio,macaddr=02:00:00:00:00:01 -net user \
+        -nographic
+
+    return $spawn_id
+}
+
+proc sh {args} {
+    foreach arg $args {
+        foreach line [split $arg "\n"] {
+            set line [string trim $line]
+            if {$line != ""} {
+                expect "# "
+                send "$line\r"
+            }
+        }
+    }
+}
